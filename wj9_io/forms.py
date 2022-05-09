277d4552ddd19418780f401310c5aebaffe2f69e
warriorjacq9 <jackflusche@gmail.com>
2022-05-08 17:24:02 -0700

    Started work on digital products.

---
+++ wj9_io/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import DigitalProduct
+
+class DigitalProductForm(forms.ModelForm):
+    class Meta:
+        model=DigitalProduct
+        fields=['name','file']
+        labels={'name':'Name', 'file':'File'}
+        widgets={'name':forms.Textarea(attrs={'cols':2,'rows':2}),'file':forms.FileField()}
