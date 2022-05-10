ff03c91bc9125b9280228a5a0a8834fd1632f8d1
warriorjacq9 <jackflusche@gmail.com>
2022-05-10 09:00:27 -0700

    Addded verification.

--- wj9_io/urls.py
+++ wj9_io/urls.py
@@ -7,6 +7,7 @@ app_name="wj9_io"
 urlpatterns=[
     path("", views.index, name='index'),
     re_path(r"^favicon\.ico$", RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'), permanent=True)),
+    re_path(r"^googlef3edb757a43765a6\.html", RedirectView.as_view(url=staticfiles_storage.url('googlef3edb757a43765a6.html'), permanent=True)),
     path("products/new_product/", views.new_product, name="new_product"),
     path("products/", views.products, name="products"),
     path("products/<int:product_id>", views.product, name="product")
